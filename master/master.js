f19793cf3d8cb2114c9b1b4e5bb113b9d502dd87
Alireza Moghise <alireza.moghise.dev@gmail.com>
2023-06-18 16:38:51 +0330

    refactor:add comments

--- master/master.js
+++ master/master.js
@@ -1,14 +1,15 @@
-const axios = require('axios');
-const readline = require('readline');
+const axios = require("axios");
+const { exit } = require("process");
+const readline = require("readline");
 
 // Tasks received from the user
 const tasks = [
-  { id: 1, command: 'ls', cpu: 1, ram: 1, disk: 1, sent: false },
-  { id: 2, command: 'ls -ltrh', cpu: 4, ram: 2, disk: 2, sent: false },
-  { id: 3, command: 'ls -ltrh', cpu: 2, ram: 2, disk: 2, sent: false },
-  { id: 4, command: 'ls -ltrh', cpu: 3, ram: 2, disk: 2, sent: false },
-  { id: 5, command: 'ls -ltrh', cpu: 7, ram: 2, disk: 2, sent: false },
-  { id: 6, command: 'ls -ltrh', cpu: 8, ram: 2, disk: 2, sent: false },
+  { id: 1, command: "ls", cpu: 1, ram: 1, disk: 1, sent: false },
+  { id: 2, command: "ls -ltrh", cpu: 4, ram: 2, disk: 2, sent: false },
+  { id: 3, command: "ls -ltrh", cpu: 2, ram: 2, disk: 2, sent: false },
+  { id: 4, command: "ls -ltrh", cpu: 3, ram: 2, disk: 2, sent: false },
+  { id: 5, command: "ls -ltrh", cpu: 7, ram: 2, disk: 2, sent: false },
+  { id: 6, command: "ls -ltrh", cpu: 8, ram: 2, disk: 2, sent: false },
 ];
 
 // Function to create a slave
@@ -24,19 +25,36 @@ const createSlave = (id, url, port, cpu, ram, disk) => {
 };
 
 // Get the slave resources from the user
-const getSlaveResources = async () => {
+const getSlave = async () => {
   const slaves = [];
-  const totalSlaves = await question('Enter the total number of slaves:');
+  const totalSlaves = await question("Enter the total number of slaves:");
 
   for (let i = 0; i < totalSlaves; i++) {
     const id = i + 1;
-    const url = await question(`Enter the IP address for Slave ${id}:`);
-    const port = parseInt(await question(`Enter the port number for Slave ${id}:`), 10);
-    const cpu = parseInt(await question(`Enter the CPU resources for Slave ${id}:`), 10);
-    const ram = parseInt(await question(`Enter the RAM resources for Slave ${id}:`), 10);
-    const disk = parseInt(await question(`Enter the disk resources for Slave ${id}:`), 10);
 
+    // get user input
+    const url = await question(`Enter the IP address for Slave ${id}:`);
+    const port = parseInt(
+      await question(`Enter the port number for Slave ${id}:`),
+      10
+    );
+    const cpu = parseInt(
+      await question(`Enter the CPU resources for Slave ${id}:`),
+      10
+    );
+    const ram = parseInt(
+      await question(`Enter the RAM resources for Slave ${id}:`),
+      10
+    );
+    const disk = parseInt(
+      await question(`Enter the disk resources for Slave ${id}:`),
+      10
+    );
+
+    // create slaves
     const slave = createSlave(id, url, port, cpu, ram, disk);
+
+    // append slaves to an array to hold info
     slaves.push(slave);
   }
 
@@ -46,7 +64,7 @@ const getSlaveResources = async () => {
 // Create readline interface
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Function to ask a question and return the user's input
@@ -60,41 +78,66 @@ const question = (text) => {
 
 // Slaves configuration
 const main = async () => {
-  const slaves = await getSlaveResources();
+  const slaves = await getSlave();
 
   // FIFO scheduling algorithm
   const scheduleTasks = async (tasks, slaves) => {
+    /* For each task in the tasks array, an asynchronous function is defined using the arrow 
+    function syntax. The asynchronous function will be executed for each task in parallel. */
     const promises = tasks.map(async (task) => {
       if (!task.sent) {
+        // get available slaves with sufficient resources
         const availableSlaves = getAvailableSlaves(task, slaves);
+
+        // if slaves do exist
         if (availableSlaves.length > 0) {
-          console.log(`Task ${task.id} scheduled on Slaves: ${availableSlaves.map(slave => slave.id).join(', ')}`);
+          console.log(
+            `Task ${task.id} scheduled on Slaves: ${availableSlaves
+              .map((slave) => slave.id)
+              .join(", ")}`
+          );
+
+          // define a flag for execution of a task
           let taskExecuted = false;
           for (const slave of availableSlaves) {
+            // send task and slave info to execute the task(response is output of bash command)
             const response = await executeTaskOnSlave(slave, task);
+
             if (response) {
-              console.log(`Task ${task.id} output from Slave ${slave.id}: ${response}`);
+              console.log(
+                `Task ${task.id} output from Slave ${slave.id}: ${response}`
+              );
+
+              // to be aware of slaves' resources
               releaseResources(slave, task);
+
+              // set send flag to true
               markTaskAsSent(task);
+
+              // set execute flag to true
               taskExecuted = true;
               break;
             }
           }
           if (!taskExecuted) {
-            console.error(`All available slaves failed to execute Task ${task.id}`);
+            console.error(
+              `All available slaves failed to execute Task ${task.id}`
+            );
           }
         } else {
+          // if slaves don't have enough resources
           console.error(`Insufficient resources to execute Task ${task.id}`);
         }
       }
     });
-
+    /*  ensures that all the asynchronous tasks represented by the 
+    promises are completed before moving forward. */
     await Promise.all(promises);
   };
 
   // Find available slaves with sufficient resources
   const getAvailableSlaves = (task, slaves) => {
-    return slaves.filter(slave => {
+    return slaves.filter((slave) => {
       return (
         slave.cpu >= task.cpu &&
         slave.ram >= task.ram &&
@@ -106,11 +149,20 @@ const main = async () => {
   // Execute task on a slave
   const executeTaskOnSlave = async (slave, task) => {
     try {
-      console.log(slave.url)
-      const response = await axios.post(`http://${slave.url}:${slave.port}/execute`, task);
+      console.log(slave.url);
+
+      // send a post request to IP address of available slave
+      const response = await axios.post(
+        `http://${slave.url}:${slave.port}/execute`,
+        task
+      );
+
+      // response data is output of bash command
       return response.data;
     } catch (error) {
-      console.error(`Error executing Task ${task.id} on Slave ${slave.id}: ${error.message}`);
+      console.error(
+        `Error executing Task ${task.id} on Slave ${slave.id}: ${error.message}`
+      );
       return null;
     }
   };
--- slave/slave.js
+++ slave/slave.js
@@ -18,6 +18,8 @@ let disk;
 
 // Prompt for slave resources
 const promptResources = () => {
+
+  // Assign resources to the slaves
   rl.question('Enter CPU resources: ', (cpuInput) => {
     cpu = parseInt(cpuInput);
     rl.question('Enter RAM resources: ', (ramInput) => {
@@ -38,6 +40,8 @@ const startServer = () => {
 
   // Endpoint for task execution
   app.post('/execute', (req, res) => {
+
+    // put body of request in a object body contains task's resources 
     const { id, command, cpu: taskCpu, ram: taskRam, disk: taskDisk } = req.body;
 
     // Check if slave has sufficient resources
@@ -57,12 +61,14 @@ const startServer = () => {
           // Task executed successfully
           console.log(`Task ${id} completed on Slave`);
 
-          // Increase slave's resources after task completion
+          // Increase slave's resources after task completion(release resources)
           cpu += taskCpu;
           ram += taskRam;
           disk += taskDisk;
+
+
           const exe_time = ((taskCpu / cpu) + (taskRam / ram) + (taskDisk / ram)) * 2000;
-          console.log(exe_time);
+          console.log(`${exe_time}ms`);
           // Delay the response by 2 seconds
           setTimeout(() => {
             res.status(200).send(stdout);
@@ -76,7 +82,10 @@ const startServer = () => {
     }
   });
 
+  // get custom port or set 3000 
   const port = process.env.PORT || 3000;
+
+  // Start the server and listen on the specified port and IP address
   app.listen(port, '0.0.0.0', () => {
     console.log(`Slave server running on 0.0.0.0:${port}`);
   });
